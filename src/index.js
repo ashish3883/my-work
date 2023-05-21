27f4915f0b059346331b41bee1d06ce67b92f9b1
Ashish <ashishkumar3883@gmail.com>
2023-05-22 00:27:33 +0530

    Routing

--- src/index.js
+++ src/index.js
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} >
+    <RouterProvider router={router}>
     <App />
     </RouterProvider>
   </React.StrictMode>
